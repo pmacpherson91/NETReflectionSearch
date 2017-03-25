9f0fb9b25366b0c115b721532c5895811b772800
pmacpherson91 <p.macpherson91@gmail.com>
2017-03-25 18:44:50 +1100

    Basic GUI

---
+++ NETReflectionSearch.Startup/DynamicFilterUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NETReflectionSearch
+{
+	/// <summary>
+	/// Interaction logic for DynamicFilterUserControl.xaml
+	/// </summary>
+	public partial class DynamicFilterUserControl : UserControl
+	{
+		public DynamicFilterUserControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
